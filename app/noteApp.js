2fa7dda2c8b49baf842f6387566ac272c8bc2622
Anthony_FAUVEL <21500369@etu.unicaen.fr>
2017-01-24 15:54:11 +0100

    Validation de 2 stories
    fonction Count() et Clear() fonctionnelle
    Le nombre de caractères restants s'actualise bien et le bouton effacer
    fonctionne bien

--- app/noteApp.js
+++ app/noteApp.js
@@ -1,4 +1,4 @@
 /**
  * Created by Anthony on 24/01/2017.
  */
-var NoteApp = angular.module('NoteApp',[]);
+var NoteApp = angular.module('NoteApp',['ngCookies']);
--- app/noteController.js
+++ app/noteController.js
@@ -3,6 +3,18 @@
  */
 
 NoteApp.controller("NoteController", function(){
+    var self = this;
+    this.messageNote ="";
+    this.sauvegarde ="";
+    this.caractere = 100;
 
-    
+    this.count = function(){
+        self.caractere = 100 - this.messageNote.length;
+    };
+
+    this.clear = function(){
+        self.messageNote = "";
+        self.caractere = 100;
+    };
+
 });
