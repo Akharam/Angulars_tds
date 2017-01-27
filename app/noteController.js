e63fabe56204cc0a1fecfed136d5f5a761f012ab
Anthony_FAUVEL <21500369@etu.unicaen.fr>
2017-01-27 23:52:45 +0100

    Travail sur les cookies et la div "Info"
    Les cookies ne sont pas créés correctement, à revoir
    Problème au niveau des différents affichages de la div "Info", continuer
    les recherches sur le data-ng-show

--- app/noteController.js
+++ app/noteController.js
@@ -2,19 +2,30 @@
  * Created by Anthony on 24/01/2017.
  */
 
-NoteApp.controller("NoteController", function(){
+NoteApp.controller("NoteController", ["$cookies", function ($cookies) {
     var self = this;
-    this.messageNote ="";
-    this.sauvegarde ="";
+    this.messageNote = "";
+    this.info = "";
     this.caractere = 100;
-
-    this.count = function(){
+    this.status = 2;
+    
+    this.count = function () {
         self.caractere = 100 - this.messageNote.length;
+        self.info ="Note modifiée";
     };
 
-    this.clear = function(){
+    this.clear = function () {
         self.messageNote = "";
         self.caractere = 100;
+        self.info="";
+    };
+    
+    this.save = function () {
+      // Retrieving a cookie
+  //var favoriteCookie = $cookies.get('myFavorite');
+  // Setting a cookie
+        $cookies.put('Note', self.messageNote);
+        self.info = "Note sauvegardée";
     };
 
-});
+}]);
