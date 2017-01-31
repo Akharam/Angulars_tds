9bc53a23c9cdcd98da1f3202c73ffdd4f804e40d
Anthony_FAUVEL <21500369@etu.unicaen.fr>
2017-01-31 15:10:33 +0100

    Début du deuxième exercice
    Création du controller et du module pour les Services, Fonction Total()
    fonctionnel

---
+++ TD1_2/app/ServicesController.js
@@ -0,0 +1,40 @@
+servicesApp.controller("ServicesController", function(){
+        
+        var self = this;
+        this.services =[
+    {
+        "name": "Web Development",
+        "price": 300,
+        "active":true
+    },{
+        "name": "Design",
+        "price": 400,
+        "active":false
+    },{
+        "name": "Integration",
+        "price": 250,
+        "active":false
+    },{
+        "name": "Formation",
+        "price": 220,
+        "active":false
+    }
+];
+    this.total = function(){
+        var count = 0;
+        for(var val in self.services){
+            if(self.services[val].active)
+                count++;
+        }
+        return count;
+    
+};
+
+    this.toggleActive = function(){
+        
+    };
+    
+                                            
+                                              
+                       
+                       });
